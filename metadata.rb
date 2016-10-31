b1aaa46c5505004bf9be5b89ee4fcabe4990ca21
Danny Guinther <dannyguinther@gmail.com>
2016-10-30 21:29:19 -0400

    Add name attribute to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+description      "Installs/Configures stable leiningen"
+license          "Apache 2.0"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 maintainer       "Max Prokopiev"
 maintainer_email "me@maxprokopiev.com"
-license          "Apache 2.0"
-description      "Installs/Configures stable leiningen"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name             "leiningen"
 version          "0.1.0"
